dcec9981619d284f6e41b134261caee31b535036
Kevin Buffardi <kbuffardi@csuchico.edu>
2017-09-06 00:32:14 -0700

    Skeleton of biggestnumber with user prompts

---
+++ biggestnumber.cpp
@@ -0,0 +1,15 @@
+//Author:
+#include<iostream>
+
+using namespace std; //so we don't need the prefix std::
+
+int main()
+{
+  
+  cout<<endl<<"Please enter a whole number: ";
+
+  cout<<endl<<"Please enter another whole number: ";
+
+
+  return 0;
+}
