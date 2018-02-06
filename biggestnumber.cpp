30f5b3a0cbf441086fb15b9419bfaaaad7221b5e
zakkuns <zkuns@mail.csuchico.edu>
2018-02-06 11:03:09 -0800

    Prompts the user to enter in two whole numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -3,12 +3,16 @@
 
 using namespace std; //so we don't need the prefix std::
 
-int main()
-{
+int main() {
+
+  int number1;
+  int number2;
   
   cout<<"Please enter a whole number:\n";
-  
+  cin >> number1;
+
   cout<<"Please enter another whole number:\n";
+  cin >> number2;
 
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
